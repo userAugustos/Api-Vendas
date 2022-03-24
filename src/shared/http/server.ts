85ba23d2a9ea12ceb613941c0d2ad2fa48b4edb8
Augustos <felilpe_augustos84@outlook.com.br>
2022-03-24 05:56:36 -0300

    feat: adding routes and express configs

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express(); // instance of express
+
+app.use(cors()); // no param in cors, so accepting all ports and requests
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => console.log('papa running'));
