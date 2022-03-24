d07377766614e6ff9c696e12bcd9e1298a9dcc6c
Augustos <felilpe_augustos84@outlook.com.br>
2022-03-24 06:33:05 -0300

    feat: creating error middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,27 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express(); // instance of express
 
 app.use(cors()); // no param in cors, so accepting all ports and requests
 app.use(express.json());
 app.use(routes);
+
+// temp. middleware to errors
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    // when error is a instance of your class, we can return directly
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(3333, () => console.log('papa running'));
