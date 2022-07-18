0ece583b7b6b03cbc6234214a26c79541e58f116
Felipe Augustos <felipe_augustos84@outlook.com.br>
2022-07-18 20:06:43 -0300

    refactor: change List Product Service options type

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,13 +1,15 @@
-import { FindConditions } from 'typeorm/find-options/FindConditions';
+import { FindConditions } from 'typeorm';
 import Product from '../typeorm/entities/product';
 import { ProductRepository } from '../typeorm/repositories/ProductRepository';
 
 export default class ListProductService {
-  public async execute(options?: FindConditions<Product>): Promise<Product[]> {
-    // passing all "FindOptions<Product>" that may we use to search Products
+  public async execute(
+    options?: FindConditions<Record<string, unknown>>
+  ): Promise<Product[]> {
+    // passing all "FindOptions<>" that may we use to search Products
     const productsRepository = new ProductRepository();
 
-    const products: Product[] = await productsRepository.find(options); //if no one is passed, method find() return all Products
+    const products = await productsRepository.find(options); //if no one is passed, method find() return all Products
 
     return products;
   }
