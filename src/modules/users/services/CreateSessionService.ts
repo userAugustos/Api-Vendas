fc08d10492a5e8889af8b5be60b6d6dcb272e29e
Felipe Augustos <felipe_augustos84@outlook.com.br>
2022-08-03 19:48:28 -0300

    feat: session service, to autenticate users

---
+++ src/modules/users/services/CreateSessionService.ts
@@ -0,0 +1,29 @@
+import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UserRepository';
+import { ISessionRequest, ISessionResponse } from '../types/autenticate';
+
+class CreateSession {
+  public async execute({ email, password }: ISessionRequest): Promise<ISessionResponse> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password', 401);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email/password', 401);
+    }
+
+    return {
+      user,
+    };
+  }
+}
+
+export default CreateSession;
